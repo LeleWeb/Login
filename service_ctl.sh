4736d694f7ddd29a17643b8b84f3dbc7c9c99c45
Alex <guanyuanchao@funguide.com.cn>
2016-05-25 10:03:59 +0800

    删除无用代码

--- service_ctl.sh
+++ service_ctl.sh
@@ -6,7 +6,7 @@ RUBY_FREE_MIN=200000
 RUBY_GC_MALLOC_LIMIT=60000000
 export RUBY_HEAP_MIN_SLOTS RUBY_FREE_MIN RUBY_GC_MALLOC_LIMIT
 
-script_dir='/project/login_system'
+script_dir='/home/alex/project/login_system'
 cd $script_dir
 
 pid="log/rainbows.pid"
